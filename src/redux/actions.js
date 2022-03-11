8619b7cd0085544daf143fb775bacc99067b7f9f
Aleksei-89ST <leha.leonov.52@bk.ru>
2022-03-11 13:52:40 +0300

    create actions

---
+++ src/redux/actions.js
@@ -0,0 +1,12 @@
+import { DECREMENT, INCREMENT } from "./types";
+
+export function incrementLikes() {
+    return {
+        type:INCREMENT
+    }
+}
+export function decrementLikes() {
+    return {
+        type:DECREMENT
+    }
+}
