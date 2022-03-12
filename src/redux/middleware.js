e785efda0f8715b4fe8cb30851a8e25044cfe75b
Aleksei-89ST <leha.leonov.52@bk.ru>
2022-03-12 14:06:43 +0300

    добавил ошибку  в middleware

--- src/redux/middleware.js
+++ src/redux/middleware.js
@@ -1,10 +1,18 @@
+import { errorOn } from "./actions";
 import { COMMENT_CREATE } from "./types";
 
-export function spamFilter(store) {
+const badWords = ["козёл", "осёл"];
+
+export function spamFilter({ dispatch }) {
   return function (next) {
     return function (action) {
       if (action.type === COMMENT_CREATE) {
-        console.log("spamFilter >>>>>", action.data.text);
+        const hasBadWords = badWords.some((res) =>
+          action.data.text.includes(res)
+        );
+        if (hasBadWords) {
+          return dispatch(errorOn("УВАЖАЙТЕ РУССКИЙ ЯЗЫК"));
+        }
       }
       return next(action);
     };
