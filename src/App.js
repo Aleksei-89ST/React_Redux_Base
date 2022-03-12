615d0cef83424534b71a667c14b30129d4dc2d6b
Aleksei-89ST <leha.leonov.52@bk.ru>
2022-03-12 13:22:41 +0300

    Добавил loader с задержкой 1 сек

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import './App.css';
 import Comments from './Comments';
 import Likes from './Likes';
+import Spin from './Spin';
 import Title from './Title';
 
 
@@ -8,6 +9,7 @@ function App() {
   return (
     <div className="App">
       <div className="wrap">
+        <Spin/>
         <div className="card">
           <div className="card-image">
             <img src="./sea.jpg" alt="surfing"/>
--- src/redux/actions.js
+++ src/redux/actions.js
@@ -1,56 +1,73 @@
-import { COMMENTS_LOAD, COMMENT_CREATE, COMMENT_DELETE, COMMENT_UPDATE, DECREMENT, INCREMENT, INPUT_TEXT } from "./types";
+import {
+  COMMENTS_LOAD,
+  COMMENT_CREATE,
+  COMMENT_DELETE,
+  COMMENT_UPDATE,
+  DECREMENT,
+  INCREMENT,
+  INPUT_TEXT,
+  LOADER_DISPLAY_OFF,
+  LOADER_DISPLAY_ON,
+} from "./types";
 
 export function incrementLikes() {
   return {
     type: INCREMENT,
-  }
+  };
 }
 export function decrementLikes() {
   return {
     type: DECREMENT,
-  }
+  };
 }
 export function inputText(text) {
   return {
     type: INPUT_TEXT,
     text,
-  }
+  };
 }
-export function commentCreate(text,id) {
+export function commentCreate(text, id) {
   return {
     type: COMMENT_CREATE,
     data: { text, id },
-  }
+  };
 }
-export function commentUpdate(text,id) {
-    return {
-      type: COMMENT_UPDATE,
-      data: { text, id },
-    }
-  }
-  export function commentDelete(id) {
-    return {
-      type: COMMENT_DELETE,
-      id
-    }
-  }
-  export function commentsLoad() {
-    return async dispatch => {
-        const response = await fetch('https://jsonplaceholder.typicode.com/comments?_limit=10')
-        const jsonData = await response.json();
-        dispatch({
-            type:COMMENTS_LOAD,
-            data:jsonData
-        })
-    }
-  }
-//   export function loaderDisplayOn() {
-//     return {
-//       type: LOADER_DISPLAY_ON,
-//     }
-//   }
-//   export function loaderDisplayOff() {
-//     return {
-//       type: LOADER_DISPLAY_OFF,
-//     }
-//   }
+export function commentUpdate(text, id) {
+  return {
+    type: COMMENT_UPDATE,
+    data: { text, id },
+  };
+}
+export function commentDelete(id) {
+  return {
+    type: COMMENT_DELETE,
+    id,
+  };
+}
+export function loaderOn() {
+  return {
+    type: LOADER_DISPLAY_ON,
+  };
+}
+export function loaderOff() {
+  return {
+    type: LOADER_DISPLAY_OFF,
+  };
+}
+export function commentsLoad() {
+  return async (dispatch) => {
+    dispatch(loaderOn());
+    const response = await fetch(
+      "https://jsonplaceholder.typicode.com/comments?_limit=10"
+    );
+    const jsonData = await response.json();
+
+    setTimeout(() => {
+      dispatch({
+        type: COMMENTS_LOAD,
+        data: jsonData,
+      });
+      dispatch(loaderOff());
+    }, 1000);
+  };
+}
